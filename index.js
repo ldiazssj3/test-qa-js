de6ee314b9a814046c2678c0af11761c18c56e12
Leonardo Diaz <leonardo.diaz@cognitionbi.com>
2021-08-10 16:47:34 -0300

    setup js test

---
+++ index.js
@@ -0,0 +1,13 @@
+const permuteUnique = (nums) => {
+  // Write your code here.
+};
+
+const simpleTest = () => {
+  console.log(permuteUnique([1, 1, 2]));
+};
+
+simpleTest();
+
+module.exports = {
+  permuteUnique,
+};
